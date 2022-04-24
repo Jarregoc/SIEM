efa01041eeafc248810e5c79bb90a6c925136a5c
Jarregoc <jarregoc@lion.lmu.edu>
2022-04-23 20:39:45 -0700

    Added communication between backend and front end

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
+let activeTabID = 0
+
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_tab_info => {
+        activeTabID = tab.tabId
         if(/^https:\/\/www\.google/.test(current_tab_info.url)) {
             chrome.tabs.insertCSS(null, {file: './mystyles.css'})
             chrome.tabs.executeScript(null, {file: './foreground.js'}, () => {
@@ -12,3 +15,13 @@ chrome.tabs.onActivated.addListener(tab => {
 // chrome.tabs.executeScript(null, {file: './foreground.js'}, () => {
 //     console.log("i injected")
 // })
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request.message === 'check the storage') {
+        chrome.tabs.sendMessage(activeTabID, {message: 'I got your message'})
+        
+        chrome.storage.local.get("password", value => {
+            console.log(value)
+        })
+    }
+})
--- foreground.js
+++ foreground.js
@@ -1,1 +1,26 @@
-document.querySelector('img.lnXdpd').classList.add('spins')
+
+const first = document.createElement('button')
+first.innerText = "SET DATA"
+first.id = "first"
+
+const second = document.createElement('button')
+second.innerText = "SHOUT OUT TO BACKEND"
+second.id = "second"
+
+document.querySelector('body').appendChild(first)
+document.querySelector('body').appendChild(second)
+
+first.addEventListener('click', () => {
+    //chrome.storage.sync
+    chrome.storage.local.set({"password" : "1234"})
+    console.log("I SET DATA")
+})
+
+second.addEventListener('click', () => {
+    chrome.runtime.sendMessage({message: 'check the storage'})
+    console.log('I sent the message for storage')
+})
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(request.message);
+})
